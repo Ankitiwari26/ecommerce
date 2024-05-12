8ec1340a9d9cb7811dd20cbc840c1b11ef0299cd
ankit <ankitiwari26@gmail.com>
2024-05-12 18:28:06 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Component/Header/Header";
 import ProductList from "./Component/Body/ProductList";
+import ModalUI from "./UI/ModalUI";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ProductList />
+
       <h1>Hello Ankit</h1>
     </div>
   );
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,28 +2,47 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import "./Header.css";
+import ModalUI from "../../UI/ModalUI";
+import { useState } from "react";
 
 const numberOfCartItem = 5;
 function Header() {
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleCartButtonClick = () => {
+    setModalShow(true);
+  };
+
+  const handleCloseModal = () => {
+    console.log("close");
+    setModalShow(false);
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Ecommerce Website</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Store</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Button variant="dark" className="cartbutton">
-              <h4>Cart</h4>
-              <div className="item_count">{numberOfCartItem}</div>
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Ecommerce Website</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Store</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Button
+                variant="dark"
+                className="cartbutton"
+                onClick={handleCartButtonClick}
+              >
+                <h4>Cart</h4>
+                <div className="item_count">{numberOfCartItem}</div>
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <ModalUI show={modalShow} onHide={handleCloseModal} />
+    </>
   );
 }
 
